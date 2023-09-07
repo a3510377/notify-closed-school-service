9cad36a8a03c1cd929f1ae17ea91a97ce33f18ab
a3510377 <a102009102009@gmail.com>
2023-09-07 20:44:19 +0800

    feat discord btn config

--- src/services/discord/commands/notify.ts
+++ src/services/discord/commands/notify.ts
@@ -1,6 +1,18 @@
-import { PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ChannelType,
+  ComponentType,
+  PermissionFlagsBits,
+  SlashCommandBuilder,
+} from 'discord.js';
 
 import { Command } from '.';
+import { TaiwanPosition } from '@/utils/variables';
+import { DiscordModel } from '@/database/discord';
+
+const baseNotifyID = 'notify-command-';
 
 const command: Command = {
   builder: new SlashCommandBuilder()
@@ -11,7 +23,74 @@ const command: Command = {
     })
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
   execute: async (_, interaction) => {
-    await interaction.reply('Not implemented');
+    const channel =
+      interaction.options.getChannel('channel') ||
+      (await interaction.client.channels.fetch(interaction.channelId));
+
+    if (channel?.type !== ChannelType.GuildText) {
+      return await interaction.reply({
+        content: '頻道必須為文字頻道歐~~',
+        ephemeral: true,
+      });
+    }
+
+    const buttons = Object.entries(TaiwanPosition).map(([key, value]) =>
+      new ButtonBuilder()
+        .setCustomId(`${baseNotifyID}pos-${key}`)
+        .setLabel(value)
+        .setStyle(ButtonStyle.Primary),
+    );
+    const allPosBtn = new ButtonBuilder()
+      .setCustomId(`${baseNotifyID}pos-all`)
+      .setLabel('全部')
+      .setStyle(ButtonStyle.Primary);
+    const closeBtn = new ButtonBuilder()
+      .setCustomId(`${baseNotifyID}off`)
+      .setLabel('關閉')
+      .setStyle(ButtonStyle.Danger);
+
+    const channelData = await DiscordModel.findOneBy({ channelID: channel.id });
+    allPosBtn.setDisabled(channelData?.city.includes('*'));
+
+    const row1 = new ActionRowBuilder<ButtonBuilder>().addComponents(buttons);
+    const row2 = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      allPosBtn,
+      closeBtn,
+    );
+
+    const response = await interaction.reply({
+      content: '設定您所需要接收的訊息',
+      components: [row1, row2],
+      ephemeral: true,
+    });
+
+    const collector = response.createMessageComponentCollector({
+      componentType: ComponentType.Button,
+      time: 1e3 * 60,
+    });
+
+    collector
+      .on('collect', async (i) => {
+        if (!i.customId.startsWith(baseNotifyID)) return;
+        const id = i.customId.substring(baseNotifyID.length);
+
+        if (id === 'off') {
+          await channelData?.remove();
+          await interaction.editReply({
+            content: `已關閉 <#${channel.id}> 停班停課通知`,
+            components: [],
+          });
+          collector.stop();
+        }
+      })
+      .on('end', async (_, reason) => {
+        if (reason === 'time') {
+          await interaction.editReply({
+            content: '一分鐘內還未收到回應，關閉交互',
+            components: [],
+          });
+        }
+      });
   },
 };
 export default command;
--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -7,36 +7,62 @@ export const TMP_FILE_PATH = 'data/tmp';
 export const DATABASE_PATH = 'data/data.db';
 
 // last update: 2023/09/04
+// https://www.taiwan.net.tw/m1.aspx?sNo=0001016
 // https://bthr.gov.taipei/News_Content.aspx
 // https://tnmao70.tainan.gov.tw/News_Content.aspx?n=10228&s=83174
 export enum TaiwanCitys {
+  // 北
   A = '臺北市',
-  B = '臺中市',
-  C = '基隆市',
-  D = '臺南市',
-  E = '高雄市',
   F = '新北市',
+  C = '基隆市',
   G = '宜蘭縣',
   H = '桃園市',
-  I = '嘉義市',
   J = '新竹縣',
-  K = '苗栗縣',
-  L = '臺中縣',
-  M = '南投縣',
-  N = '彰化縣',
   O = '新竹市',
+
+  // 中
+  B = '臺中市',
+  K = '苗栗縣',
+  N = '彰化縣',
+  M = '南投縣',
   P = '雲林縣',
+
+  // 南
+  D = '臺南市',
+  E = '高雄市',
+  I = '嘉義市',
   Q = '嘉義縣',
-  R = '臺南縣',
-  S = '高雄縣',
   T = '屏東縣',
+
+  // 東
   U = '花蓮縣',
   V = '臺東縣',
+
+  // 離島
   W = '金門縣',
   X = '澎湖縣',
-  Y = '陽明山',
   Z = '連江縣',
 }
 
+export enum TaiwanPosition {
+  north = '北部',
+  central = '中部',
+  south = '南部',
+  east = '東部',
+  outlyingIslands = '離島',
+}
+
+export const TaiwanCitysDistributed: Record<
+  TaiwanPositionType,
+  TaiwanCityKeyType[]
+> = {
+  north: ['A', 'F', 'C', 'G', 'H', 'J', 'O'],
+  central: ['B', 'K', 'N', 'M', 'P'],
+  south: ['D', 'E', 'I', 'Q', 'T'],
+  east: ['U', 'V'],
+  outlyingIslands: ['W', 'X', 'Z'],
+};
+
+export type TaiwanPositionType = keyof typeof TaiwanPosition;
 export type TaiwanCityKeyType = keyof typeof TaiwanCitys;
 export const TaiwanCitysMap = enumToDict(TaiwanCitys);
